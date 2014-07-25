d5a1aaa1720c0b5a256c34062424d83706d0efc0
Eric Lin <ericlin@axiomzen.co>
2014-07-25 14:10:11 -0700

    initial commit

---
+++ master.rb
@@ -0,0 +1,108 @@
+class Code
+  def initialize(answer_code="RBGY")
+    @answer_code = answer_code
+  end
+
+  def self.random
+    colors = []
+    4.times do
+      colors << ['R','B','G','Y'].sample
+    end
+    # for i in 0..3
+    #   colors << ['R','B','G','Y'].sample
+    # end
+    Code.new(colors.join(''))
+  end
+
+  def self.sanitize(code)
+    if code.scan(/[rgbyRGBY]+/).join('').length == 4
+      return code.scan(/[rgbyRGBY]+/).join('').capitalize
+    end
+    false
+  end
+
+  def code
+    @answer_code
+  end
+
+  def count_colors(code)
+    count_colors = Hash.new(0)
+
+    code.split('').each do |col|
+      count_colors[col] += 1
+    end
+
+    count_colors
+  end
+
+  def exact_matches(code)
+    total = 0
+    for i in 0..code.length-1
+      if @answer_code[i] == code[i]
+        total += 1
+      end
+    end
+    total
+  end
+
+  def near_matches(code)
+    total = 0
+
+    answer_colors = count_colors(@answer_code)
+    player_colors = count_colors(code)
+
+    player_colors.each do |col, val|
+      # total += [answer_colors[col], val].min
+      if answer_colors[col] > val
+        total += val
+      else
+        total += answer_colors[col]
+      end
+    end
+
+    total - exact_matches(code)
+  end
+end
+
+class Game
+  attr_reader :answer
+
+  MAX_TURNS = 5
+
+  def initialize(answer = Code.random)
+    if answer.class == Code
+      @answer = answer
+    else
+      @answer = Code.new(answer)
+    end
+  end
+
+  def play
+    MAX_TURNS.times do
+      new_guess = get_guess
+      display_matches(new_guess)
+      if win?(new_guess)
+        puts "You win!"
+        return
+      end
+    end
+
+    puts "You lose, the answer was #{@answer.code}!"
+  end
+
+  def get_guess
+    puts "What's your guess?"
+    gets.chomp
+  end
+
+  def display_matches(guess)
+    exact_matches = @answer.exact_matches(guess)
+    near_matches = @answer.near_matches(guess)
+
+    [exact_matches, near_matches]
+  end
+
+  def win?(guess)
+    @answer.exact_matches(guess) == 4
+  end
+end
---
+++ server.rb
@@ -0,0 +1,59 @@
+require 'sinatra'
+require 'json'
+require 'securerandom'
+require 'mongo'
+require 'sinatra/reloader'
+
+require './master.rb'
+
+include Mongo
+
+conn = MongoClient.new
+db = conn['eric-mastermind']
+coll = db['games']
+
+post('/new_game') do
+  content_type :json
+
+  new_game_key = SecureRandom.urlsafe_base64(50)
+  new_code = Game.new.answer.code
+
+  unless params['user']
+    return { :error => "Please provide a user parameter in your post request"}
+  end
+
+  coll.insert({ :user => params[:user], :game_key => new_game_key, :num_guesses => 0, :answer_code => new_code, :past_guesses => [], :past_results => [] })
+  { :game_key => new_game_key, :num_guesses => 0, :past_guesses => [], :past_results => [] }.to_json
+end
+
+post('/guess') do
+  content_type :json
+  unless params['game_key']
+    return { :error => "Could not find game key. Please post with proper key!" }.to_json
+  end
+
+  player_guess = Code.sanitize(params['code'])
+
+  unless params['code'] && player_guess
+    return { :error => "Invalid code submission. Please post with code consisting of 4 letters: RBGY" }.to_json
+  end
+
+  game_key = params['game_key']
+  game = coll.find({ 'game_key' => game_key }).first()
+
+  past_guesses = game['past_guesses'] << player_guess
+  num_guesses = game['num_guesses'] + 1
+  answer_code = game['answer_code']
+
+  game_object = Game.new(answer_code)
+
+  result = game_object.display_matches(player_guess)
+  past_results = game['past_results'] << result
+
+  if game_object.win?(player_guess)
+    return { :user => game['user'], :game_key => game_key, :num_guesses => num_guesses, :past_guesses => past_guesses, :past_results => past_results, :guess => player_guess, :result => "You win!" }.to_json
+  end
+
+  coll.update({ 'game_key' => game_key }, {:user => game['user'], :game_key => game_key, :answer_code => answer_code, :num_guesses => num_guesses, :past_guesses => past_guesses, :past_results => past_results })
+  { :game_key => game_key, :num_guesses => num_guesses, :past_guesses => past_guesses, :past_results => past_results, :guess => player_guess, :result => ["You got #{result[0]} exact!","You got #{result[1]} near!"] }.to_json
+end
